3bcdcea0c1c283d7fe548565da273e695054c29e
Pedro Vieira <139348444+PedroVieiraC@users.noreply.github.com>
2024-02-06 17:07:57 -0300

    Delete C/Cstand directory

--- C/Cstand/multi.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Digite 2 variáveis inteiras\n");
-    int x, y;
-    scanf("%i %i", &x, &y);
-    printf("O resultado da multiplicação é: %i\n", x*y);
-    return 0;
-}
