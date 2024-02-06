b2b9ae8ea2ee01face0be9b42c887cd7d49d98ca
PedroVieiraC <pedrovieiratrab@gmail.com>
2024-02-06 17:05:49 -0300

    renomeando pastas por nome de curso

---
+++ C/C CALDL/multi.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Digite 2 variáveis inteiras\n");
+    int x, y;
+    scanf("%i %i", &x, &y);
+    printf("O resultado da multiplicação é: %i\n", x*y);
+    return 0;
+}
