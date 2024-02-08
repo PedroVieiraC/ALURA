a053f1de1dc44e1c6f81bf8f9f6f9784afd36439
PedroVieiraC <pedrovieiratrab@gmail.com>
2024-02-08 18:18:21 -0300

    need to fix drawer and miss counter cause it is change with tryes against change when it's wrong only

--- C/C ANL/forca.c
+++ C/C ANL/forca.c
@@ -3,14 +3,15 @@
 #include <math.h>
 #include <string.h>
 #include <ctype.h>
+#include "header.h"
 
-typedef struct
-{
-} Controle;
+char secretword[20];
+char guess_storage[26];
+int tryes = 0,misstryes=0, limit, difficulty;
 
-void welcome()
+//function to give welcome to user 
+void start()
 {
-    printf("Welcome to hangman game!!!\n");
     printf("      +---+       \n");
     printf("      |   |       \n");
     printf("      O   |       \n");
@@ -18,82 +19,222 @@ void welcome()
     printf("     / \\  |       \n");
     printf("          |       \n");
     printf("    ========='''] \n");
+    printf("Welcome to hangman game!!!\n");
+    pressets();
+}
+//function to set difficult 
+void pressets(){
+    printf("Choose the difficulty. \n(1)Easy (2)Medium (3)Hard\n");
+    scanf("%i", &difficulty);
+    switch (difficulty)
+    {
+    case 1:
+        limit = 5;
+        break;
+    case 2:
+        limit = 10;
+        break;
+    case 3:
+        limit = 20;
+        break;
+    default: limit = 1;
+        break;
+    }
+}
+//void to choose the secret word
+void secretwordchoose()
+{
+    sprintf(secretword, "teste");
 }
 
-char fguess(char guess, char guess_storage[], int *tryes)
+void missdrawer(){
+    printf("misst %i\n",misstryes);
+    if((misstryes == 0 && difficulty==1) ||(misstryes == 0 && difficulty==2) ||(misstryes == 0 && difficulty==3) ){
+    printf("        +---+             \n"); 
+    printf("        |   |             \n");  
+    printf("        O   |             \n");
+    printf("            |             \n");  
+    printf("            |             \n");  
+    printf("            |             \n");  
+    printf("        =========''', ''' \n");
+    }
+    if((misstryes == 1 && difficulty==1) ||(misstryes == 2 && difficulty==2) ||(misstryes == 4 && difficulty==3) ){
+    printf("        +---+             \n"); 
+    printf("        |   |             \n");  
+    printf("        O   |             \n");
+    printf("        |   |             \n");  
+    printf("            |             \n");  
+    printf("            |             \n");  
+    printf("        =========''', ''' \n");
+    }
+    if((misstryes == 2 && difficulty==1) ||(misstryes == 4 && difficulty==2) ||(misstryes == 8 && difficulty==3) ){
+    printf("        +---+             \n"); 
+    printf("        |   |             \n");  
+    printf("        O   |             \n");
+    printf("       /|   |             \n");  
+    printf("            |             \n");  
+    printf("            |             \n");  
+    printf("        =========''', ''' \n");
+    }
+    if((misstryes == 3 && difficulty==1) ||(misstryes == 6 && difficulty==2) ||(misstryes == 12 && difficulty==3)) {
+    printf("        +---+             \n"); 
+    printf("        |   |             \n");  
+    printf("        O   |             \n");
+    printf("       /|\\  |             \n");  
+    printf("            |             \n");  
+    printf("            |             \n");  
+    printf("        =========''', ''' \n");
+    }
+    if((misstryes == 4 && difficulty==1) ||(misstryes == 8 && difficulty==2) ||(misstryes == 16 && difficulty==3) ){
+    printf("        +---+             \n"); 
+    printf("        |   |             \n");  
+    printf("        O   |             \n");
+    printf("       /|\\  |             \n");  
+    printf("       /    |             \n");  
+    printf("            |             \n");  
+    printf("        =========''', ''' \n");
+    }
+    if((tryes == 5 && difficulty==1) ||(tryes == 10 && difficulty==2) ||(tryes == 20 && difficulty==3) ){
+    printf("        +---+             \n"); 
+    printf("        |   |             \n");  
+    printf("        O   |             \n");
+    printf("       /|\\  |             \n");  
+    printf("       / \\  |             \n");  
+    printf("            |             \n");  
+    printf("        =========''', ''' \n");
+    }
+
+    
+
+}
+//function that manage the game work
+void game()
 {
+    printf("You already gave %i guesses\n", tryes);
+
+    for (int i = 0; i < strlen(secretword); i++)
+    {
+        // if found, prints the letter, else print the underscore
+        if (alrguessed(secretword[i]))
+            printf("%c", secretword[i]);
+        else
+            printf("_");
+    }
+    printf("\n");
+}
+
+// function to guess a letter in the game
+void fguess()
+{
+    char guess;
+    printf("Try a letter: ");
     scanf(" %c", &guess);
     guess = tolower(guess);
 
-    guess_storage[*tryes] = guess;
-    *tryes = *tryes + 1;
-
-    return guess;
+    guess_storage[tryes] = guess;
+    tryes++;
 }
 
-void alrguessed(int* found, char guess_storage[], char secretword[], int tryes, int i)
+// function to verify if the guess are in the secret word
+int alrguessed(char letter)
 {
+    int found = 0;
     for (int j = 0; j < tryes; j++)
     {
-
-        if (guess_storage[j] == secretword[i])
+        if (guess_storage[j] == letter)
         {
-            *found = 1;
+            found = 1;
             break;
         }
     }
+    return found;
+}
+
+// function to see if the misses are the same as the limit
+int lose()
+{
+    int miss = 0;
+    for (int i = 0; i < tryes; i++)
+    {
+        int exist = 0;
+
+        for (int j = 0; j < strlen(secretword); j++)
+        {
+            if (guess_storage[i] == secretword[j])
+            {
+                exist = 1;
+                break;
+            }
+        }
+        if (!exist)
+            miss++;
+    }
+    printf("miss = %i\n", miss);
+    return miss >= limit;
+}
+//miss counter
+int misscounter()
+{
+    for (int i = 0; i < tryes; i++)
+    {
+        int exist = 0;
+
+        for (int j = 0; j < strlen(secretword); j++)
+        {
+            if (guess_storage[i] == secretword[j])
+            {
+                exist = 1;
+                break;
+            }
+        }
+        if (!exist)
+            misstryes++;
+    }
+    
+}
+
+// function to see if all the letters of sw was already guessed
+int win()
+{
+    for (int i = 0; i < strlen(secretword); i++)
+    {
+        if (!alrguessed(secretword[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int main()
 {
     // declaração palavra secreta
 
-    char secretword[20];
+    int hit = 0;
 
-    // write in
-    sprintf(secretword, "teste");
+    start();
 
-    int hit = 0, lose = 0;
-
-    char guess_storage[26];
-    int tryes = 0;
-
-    // welcome
-    welcome();
+    secretwordchoose(secretword);
 
     do
     {
-        for (int i = 0; i < strlen(secretword); i++)
-        {
+        game();
+        fguess();
+        misscounter();
+        missdrawer();
+        
+    } while (!win() && !lose());
 
-            int found = 0;
-
-            //changes found value to 1 if the letter was already used
-            alrguessed(&found,guess_storage,secretword,tryes,i);
-
-            //if found, prints the letter, else print the underscore
-            if (found)
-                printf("%c", secretword[i]);
-            else
-                printf("_");
-        }
-        printf("\n");
-
-        //start a char and store the letter in the guess array
-        char guess;
-        fguess(guess, guess_storage, &tryes);
-
-        for (int i = 0; i < strlen(secretword); i++)
-        {
-            if (guess == secretword[i])
-            {
-                printf("letter %c in %i position\n", guess, i + 1);
-            }
-        }
-
-    } while (!hit && !lose);
-
-    // printf("%s",secretword);
+    if (win()){
+        missdrawer();
+        printf("YOU WIN!!!\n");
+        printf("secret word was %s\n",secretword);
+    }
+    else
+    {
+        missdrawer();
+        printf("YOU LOSE, TRY AGAIN!\n");
+    }
 
     return 0;
 }
---
+++ C/C ANL/header.h
@@ -0,0 +1,9 @@
+void start();
+void pressets();
+void secretwordchoose();
+void missdrawer();
+void game();
+void fguess();
+int alrguessed(char letter);
+int lose();
+int win();
