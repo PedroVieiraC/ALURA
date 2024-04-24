b88f5dda91031cbb13841318fbc64a9f88850bd7
PedroVieiraC <pedrovieiratrab@gmail.com>
2024-04-23 21:10:34 -0300

    testadd

---
+++ JAVA/challenge/src/vieira/pedro/module/Audio.java
@@ -0,0 +1,17 @@
+package vieira.pedro.module;
+
+public class Audio {
+    private String title;
+    private int durationTime;
+    private int plays;
+    private int likes;
+    private double rate;
+
+    public void like(){
+        this.likes++;
+    }
+
+    public void listen(){
+
+    }
+}
