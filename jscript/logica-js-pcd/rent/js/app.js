91d65a26fb732c58d6788d816118ea65f5081355
PedroVieiraC <pedrovieiratrab@gmail.com>
2024-06-01 11:58:16 -0300

    clean code

--- jscript/logica-js-pcd/rent/js/app.js
+++ jscript/logica-js-pcd/rent/js/app.js
@@ -6,21 +6,10 @@ function alterarStatus(id) {
   if (img.classList.contains("dashboard__item__img--rented")) {
     img.classList.remove("dashboard__item__img--rented");
     button.classList.remove("dashboard__item__button--return");
-    button.textContent = 'Alugar';
+    button.textContent = "Alugar";
   } else {
     img.classList.add("dashboard__item__img--rented");
     button.classList.add("dashboard__item__button--return");
-    button.textContent = 'Devolver';
-}
-}
-
-function changeBtn() {
-  let btn = document.getElementById("game-1");
-  if (btn.classList.contains("container__botao-desabilitado")) {
-    btn.classList.remove("container__botao-desabilitado");
-    btn.classList.add("container__botao");
-  } else {
-    btn.classList.remove("container__botao");
-    btn.classList.add("container__botao-desabilitado");
+    button.textContent = "Devolver";
   }
 }
